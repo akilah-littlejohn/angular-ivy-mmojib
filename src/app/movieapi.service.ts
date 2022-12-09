aa95fc25a22e50c1ac8191d7a52056d84fd08e75
akilah-littlejohn <akilahlittlejohn@gmail.com>
2022-12-09 15:44:56 -0600

    1 file modified

--- src/app/movieapi.service.ts
+++ src/app/movieapi.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { from, Observable } from 'rxjs';
+
+import { MovieData } from './movie-data';
 @Injectable()
 export class MovieapiService {
 
 movieDataUrl:string = '';
 
-  constructor(hp:HttpClient) { }
+  constructor(public hp:HttpClient) { }
 
+getMovie(): Observable<MovieData[]> {
+  return this.hp.get<MovieData[]>(this.movieDataUrl)
+}
 }
